feda0f8552a65803bc33ee444c25f03b2517fff0
S <s>
2017-03-04 14:39:36 +0800

    {"types":"C","runs":108}

--- NameSurferGraph.java
+++ NameSurferGraph.java
@@ -89,8 +89,29 @@ public class NameSurferGraph extends GCanvas
 
 
     private void drawFixedLabel() {
-        one_lab = new GLabel("1900", 0, APPLICATION_HEIGHT);
+        int spread = APPLICATION_WIDTH/11;
+        zero_lab = new GLabel("1900", 0, APPLICATION_HEIGHT);
+        add(zero_lab);
+        one_lab = new GLabel("1910", spread, APPLICATION_HEIGHT);
         add(one_lab);
+        two_lab = new GLabel("1920", 2*spread, APPLICATION_HEIGHT);
+        add(two_lab);
+        three_lab = new GLabel("1930", 3*spread, APPLICATION_HEIGHT);
+        add(three_lab);
+        four_lab = new GLabel("1940", 4*spread, APPLICATION_HEIGHT);
+        add(four_lab);
+        five_lab = new GLabel("1950", 5*spread, APPLICATION_HEIGHT);
+        add(five_lab);
+        six_lab = new GLabel("1960", 6*spread, APPLICATION_HEIGHT);
+        add(six_lab);
+        seven_lab = new GLabel("1970", 7*spread, APPLICATION_HEIGHT);
+        add(seven_lab);
+        eight_lab = new GLabel("1980", 0, APPLICATION_HEIGHT);
+        add(eight_lab);
+        nine_lab = new GLabel("1990", 0, APPLICATION_HEIGHT);
+        add(nine_lab);
+        ten_lab = new GLabel("2000", 10*spread, APPLICATION_HEIGHT);
+        add(ten_lab);
     }
 	
 
@@ -108,7 +129,18 @@ public class NameSurferGraph extends GCanvas
     private GLine eight;
     private GLine nine;
     private GLine ten;
+    private GLabel zero_lab;
     private GLabel one_lab;
+    private GLabel two_lab;
+    private GLabel three_lab;
+    private GLabel four_lab;
+    private GLabel five_lab;
+    private GLabel six_lab;
+    private GLabel seven_lab;
+    private GLabel eight_lab;
+    private GLabel nine_lab;
+    private GLabel ten_lab;    
+
 
     
     /* Implementation of the ComponentListener interface */
