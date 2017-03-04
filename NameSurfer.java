a9e43603a96f5cab23bb4ed2845f39fa8a24d77a
S <s>
2017-03-04 08:37:23 +0800

    {"types":"C","runs":77}

--- NameSurfer.java
+++ NameSurfer.java
@@ -11,22 +11,27 @@ import javax.swing.*;
 
 public class NameSurfer extends ConsoleProgram implements NameSurferConstants {
 
-/* Method: init() */
-/**
- * This method has the responsibility for reading in the data base
- * and initializing the interactors at the bottom of the window.
- */
-	public void init() {
-	    // You fill this in, along with any helper methods //
-	}
+    /* Method: init() */
+    /**
+     * This method has the responsibility for reading in the data base
+     * and initializing the interactors at the bottom of the window.
+     */
+    public void init() {
+        add(new JButton("Clear"), SOUTH);
+    }
 
-/* Method: actionPerformed(e) */
-/**
- * This class is responsible for detecting when the buttons are
- * clicked, so you will have to define a method to respond to
- * button actions.
- */
-	public void actionPerformed(ActionEvent e) {
-		// You fill this in //
-	}
+    /* Method: actionPerformed(e) */
+    /**
+     * This class is responsible for detecting when the buttons are
+     * clicked, so you will have to define a method to respond to
+     * button actions.
+     */
+    public void actionPerformed(ActionEvent e) {
+        // You fill this in //
+    }
+
+
+    /* Instance variables */
+    
+    
 }
